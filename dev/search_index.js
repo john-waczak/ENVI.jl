90c138672e4fe656cf5f2a6746e2a644ccfe628f
Documenter.jl <documenter@juliadocs.github.io>
2022-12-27 22:44:31 +0000

    build based on d5c4528

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ENVI","category":"page"},{"location":"#ENVI","page":"Home","title":"ENVI","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ENVI.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ENVI]","category":"page"}]
+}
